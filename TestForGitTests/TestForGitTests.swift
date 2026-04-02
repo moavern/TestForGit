e8f2b077484efd586edac125111bbc1219ed06d5
Mohammed Chauhan <crimsonavern@gmail.com>
2026-04-02 13:09:17 -0500

    Initial Commit

---
+++ TestForGitTests/TestForGitTests.swift
@@ -0,0 +1,17 @@
+//
+//  TestForGitTests.swift
+//  TestForGitTests
+//
+//  Created by Mohammed Avern on 4/2/26.
+//
+
+import Testing
+@testable import TestForGit
+
+struct TestForGitTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
